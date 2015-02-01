8bab963431d37a80abd35ddd4a40316acaaa0568
tomas.krivda <tkrivda@engage3.com>
2015-02-01 21:52:59 +0100

    added namelable

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,10 +12,10 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +32,16 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-    
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.purpleColor()
         
         enterMessageTextField.text = ""
+        enterNameTextField.text = ""
+
         enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
     
         mailButton.setTitle("Mail Sent!", forState: UIControlState.Normal)
         
